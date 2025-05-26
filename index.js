e04c06e74cb7e7290a179325f6aaa9d5eb63d02f
vilchu <55512609+vilchu@users.noreply.github.com>
2025-05-26 22:29:44 +0300

    added css file

---
+++ index.js
@@ -0,0 +1,38 @@
+  function fadeOut(element, callback) {
+    element.classList.add("fade-out");
+    setTimeout(() => {
+      element.classList.add("hidden");
+      element.classList.remove("fade-out");
+      callback();
+    }, 500);
+  }
+
+  function goToSnippets() {
+    const home = document.getElementById("home");
+    const snippets = document.getElementById("snippets");
+    const backButton = document.getElementById("backButton");
+
+    fadeOut(home, () => {
+      snippets.classList.remove("hidden");
+      snippets.style.opacity = 0;
+      setTimeout(() => {
+        snippets.style.opacity = 1;
+      }, 10);
+      backButton.classList.remove("hidden");
+    });
+  }
+
+  function goBackHome() {
+    const home = document.getElementById("home");
+    const snippets = document.getElementById("snippets");
+    const backButton = document.getElementById("backButton");
+
+    fadeOut(snippets, () => {
+      home.classList.remove("hidden");
+      home.style.opacity = 0;
+      setTimeout(() => {
+        home.style.opacity = 1;
+      }, 10);
+      backButton.classList.add("hidden");
+    });
+  }
